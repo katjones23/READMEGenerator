94414f84329d483e487dc89c7ae1d79e76d06f62
Katherine Jones <kejone1414@gmail.com>
2020-04-28 19:48:49 -0400

    feat: prompts added

--- index.js
+++ index.js
@@ -4,13 +4,13 @@
 // * Table of Contents
 // * Installation
 // * Usage
+// *credits
 // * License
 // * Contributing
 // * Tests
 // * Questions
 //   * User GitHub profile picture
 //   * User GitHub email
-// Refer to template mentioned in readme
 
 const inquirer = require('inquirer')
 const api = require('./utils/api.js')
@@ -19,14 +19,54 @@ const questions = [
     {
         type: 'input',
         name: 'username',
-        message: 'What is your GitHub username?',
+        message: 'What is your GitHub username?'
     },
+    {
+        type: 'input',
+        name: 'projectTitle',
+        message: 'What is the project title?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What is the project description?'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the installation instructions?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What is the usage information?'
+    },
+    {
+        type: 'input',
+        name: 'license',
+        message: 'What license should be used?'
+    },
+    {
+        type: 'input',
+        name: 'collaborators',
+        message: 'Did you have any collaborators?'
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'What should contributors know?'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Are there any tests to run?'
+    }
 ];
 
 inquirer.prompt(questions).then(answers => {
     async function run() {
         let { email, picture } = await api(answers.username);
-        console.log(email, picture)
+        console.log(email, picture);
     }
 
     run();
